1ea8aa1efec21be8e33d2d1eee36a2df48412d20
CedricXing <cedricxing0513@gmail.com>
2016-12-05 20:31:17 +0800

    modify Client.java and Server.java

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,10 @@
  * Created by cedricxing on 2016/11/22.
  */
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
@@ -9,6 +13,16 @@ import java.awt.event.*;
 import javax.swing.border.*;
 
 public class Client extends JFrame{
+    private String client_name;
+
+    private JPanel up_panel;//up panel
+
+    private JPanel set_panel;//login in and set
+    private JLabel set_label;
+    private ImageIcon set_icon = new ImageIcon("set.png");
+    private JLabel user_label;
+    private ImageIcon user_icon = new ImageIcon("user.png");
+
     private JPanel search_panel; //Panel to search
     private JTextField search_field;//Panel to input words
     private JLabel search_label;
@@ -18,12 +32,12 @@ public class Client extends JFrame{
     private JPanel main_panel;//Panel to Display explaination
     private JPanel explaination_panel;
     private JPanel check_box;
-    private JPanel jp_baidu;
+    private JPanel jp_biying;
     private JPanel jp_youdao;
-    private JPanel jp_google;
-    private JCheckBox jch_baidu;
+    private JPanel jp_jinshan;
+    private JCheckBox jch_biying;
     private JCheckBox jch_youdao;
-    private JCheckBox jch_google;
+    private JCheckBox jch_jinshan;
 
     private Border line_border = new LineBorder(Color.LIGHT_GRAY,1);
 
@@ -31,22 +45,79 @@ public class Client extends JFrame{
 
     final private String default_string = new String("search...");
 
-    public Client(){
-        init_search_panel();
-        init_main_panel();
+    ObjectOutputStream to_server;
+    ObjectInputStream from_server;
 
-        this.add(search_panel,BorderLayout.NORTH);
+    public Client(){
+        //this.to_server = to_server;
+        //this.from_server = from_server;
+
+        init_search_panel();
+        init_set_panel();
+        init_main_panel();
+        init_up_panel();
+
+        this.add(up_panel,BorderLayout.NORTH);
         this.add(main_panel,BorderLayout.CENTER);
 
-        this.setTitle("Online Dictionary");
+        this.setTitle("Welcome ");
         this.setSize(800,600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        try{
+            Socket socket = new Socket("localhost",8000);
+            to_server = new ObjectOutputStream(socket.getOutputStream());
+            from_server = new ObjectInputStream(socket.getInputStream());
+        }
+        catch (IOException ex){
+            System.out.println(ex);
+        }
+    }
+
+    private void init_set_panel(){
+        set_panel = new JPanel(new FlowLayout(FlowLayout.RIGHT,5,10));
+
+        set_label = new JLabel(set_icon);
+        user_label = new JLabel(user_icon);
+
+        user_label.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new Client_Login(to_server,from_server);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+
+        set_panel.add(user_label);
+        set_panel.add(set_label);
+
+    }
+
+    private void init_up_panel(){
+        up_panel = new JPanel(new GridLayout(1,2));
+
+        up_panel.add(search_panel);
+        if(set_panel == null)
+            System.out.println("h");
+        up_panel.add(set_panel);
+
     }
 
     private void init_search_panel(){
-        search_panel = new JPanel(new FlowLayout(FlowLayout.RIGHT,5,10));
+        search_panel = new JPanel(new FlowLayout(FlowLayout.CENTER,5,10));
 
         search_field = new JTextField(default_string,20);//set default string
         search_field.setForeground(Color.black);//set color
@@ -63,6 +134,41 @@ public class Client extends JFrame{
             public void mouseReleased(MouseEvent e){}
         });
 
+        search_field.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    try {
+                        String word = search_field.getText();
+                        //to_server.writeObject(word);
+                        //to_server.flush();
+                    } catch (Exception ex) {
+                        System.out.println(ex);
+                    }
+                }
+            }
+
+
+            @Override
+            public void keyReleased(KeyEvent e){
+                    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                        try {
+                            String word = search_field.getText();
+                            //to_server.writeObject(word);
+                            //to_server.flush();
+                        } catch (Exception ex) {
+                            System.out.println(ex);
+                        }
+                    }
+                }
+        });
+
+
         search_label = new JLabel(search_icon);
         search_label.setBorder(line_border);
         search_label.addMouseListener(new MouseListener() {
@@ -77,9 +183,7 @@ public class Client extends JFrame{
 
         search_panel.add(search_field);
         search_panel.add(search_label);
-        //search_panel.add(new JLabel(add_client_icon));
 
-        search_panel.setBorder(line_border);
     }
 
     private void init_main_panel(){
@@ -87,23 +191,23 @@ public class Client extends JFrame{
         main_panel.setLayout(new BorderLayout(0,0));
 
         check_box = new JPanel(new FlowLayout(FlowLayout.CENTER,180,10));
-        jch_baidu = new JCheckBox("BaiDu");
-        jch_google = new JCheckBox("Google");
+        jch_biying = new JCheckBox("BiYing");
+        jch_jinshan = new JCheckBox("JinShan");
         jch_youdao = new JCheckBox("YouDao");
 
-        check_box.add(jch_baidu);
-        check_box.add(jch_google);
+        check_box.add(jch_biying);
+        check_box.add(jch_jinshan);
         check_box.add(jch_youdao);
         check_box.setBorder(line_border);
 
         explaination_panel = new JPanel(new GridLayout(1,1));
 
-        jp_baidu = new JPanel();jp_baidu.setBorder(line_border);
+        jp_biying = new JPanel();jp_biying.setBorder(line_border);
         jp_youdao = new JPanel();jp_youdao.setBorder(line_border);
-        jp_google = new JPanel();jp_google.setBorder(line_border);
+        jp_jinshan = new JPanel();jp_jinshan.setBorder(line_border);
 
-        explaination_panel.add(jp_baidu);
-        explaination_panel.add(jp_google);
+        explaination_panel.add(jp_biying);
+        explaination_panel.add(jp_jinshan);
         explaination_panel.add(jp_youdao);
 
 
--- src/Server.java
+++ src/Server.java
@@ -2,7 +2,63 @@
  * Created by cedricxing on 2016/11/22.
  */
 import javax.swing.*;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class Server extends JFrame{
+public class Server {
+    public Server(){
+        try{
+            ServerSocket serversocket = new ServerSocket(8000);//port number 8000
 
+            //int client_no = 1;
+
+            while(true){
+                Socket socket = serversocket.accept();
+
+                InetAddress inetAddress = socket.getInetAddress();
+
+                HandleClient client_task = new HandleClient(socket);
+
+                new Thread(client_task).start();
+
+            }
+
+        }
+        catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+
+    public static void main(String[] args){
+        new Server();
+    }
+}
+
+class HandleClient implements Runnable{
+    private Socket socket;
+
+    public HandleClient(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try{
+            ObjectInputStream inputFromClient = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream outputToClient = new ObjectOutputStream(socket.getOutputStream());
+
+            while(true){
+                String mess = (String)inputFromClient.readObject();
+                String []info = mess.split("[:]");
+                if(info[0].equals("register")){
+
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
