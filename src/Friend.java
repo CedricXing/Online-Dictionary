2779da5fc2c6ee186722bef78fc2cba3adec7872
CedricXing <cedricxing0513@gmail.com>
2016-12-16 21:23:33 +0800

    add friend.java

---
+++ src/Friend.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+/**
+ * Created by cedricxing on 2016/12/16.
+ */
+public class Friend extends JFrame {
+
+}
